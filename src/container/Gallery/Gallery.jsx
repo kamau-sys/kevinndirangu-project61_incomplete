77f69cd8de6a71743f655698149bfe938d194eca
kamau-sys <kevinndirangu6@gmail.com>
2025-04-28 17:32:51 +0000

    ...

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from 'react'
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
+import {BsInstagram,BsArrowLeftShort,BsArrowRightShort} from "react-icons/bs"
+import "./Gallery.scss"
 
-import './Gallery.css';
+const Gallery = () => {
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
 
-export default Gallery;
+  return (
+    <div className='app__gallery flex__center'>
+      <div className="app__gallery-content">
+        <SubHeading title = "Instagram"/>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Gallery
